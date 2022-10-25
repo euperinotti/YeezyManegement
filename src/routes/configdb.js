7c237692ebfc4d7dd168035378a2b418640c17d0
Allysom Renczenczen <allysom.renczenczen18@gmail.com>
2022-10-25 15:30:12 -0300

    Configurando o banco dados no nosso projeto

---
+++ src/routes/configdb.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCQOOzm2eydhNpucb6CfDFqXt1ewInINL8",
+    authDomain: "yeezymanegement-30bf7.firebaseapp.com",
+    projectId: "yeezymanegement-30bf7",
+    storageBucket: "yeezymanegement-30bf7.appspot.com",
+    messagingSenderId: "765236544589",
+    appId: "1:765236544589:web:d24fae07591cc9d53b0804",
+    measurementId: "G-J3PDX681H4"
+})
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp)
+
+onAuthStateChanged(auth, user =>{
+    if (user != null){
+        console.log("Logado com sucesso!");
+    }else{
+        console.log("Algo deu errado");
+    }
+})
--- src/server.js
+++ src/server.js
@@ -7,19 +7,6 @@ const app = express();
 
 app.use(express.json())
 
-
-
-
-
-
-/*
-route.get('/', (req, res) => {
-    // res.send(`gay kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk`);
-    res.json({
-        message: "mal vindo"
-    });
-})*/
-
 app.listen(5400, () => {
     console.log("API rodando na porta 3000 e banco na porta 5432")
 })
