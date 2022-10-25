84edb8e21faafe5e4ee85f90ea4900d2f526fc91
Allysom Renczenczen <allysom.renczenczen18@gmail.com>
2022-10-25 14:32:05 -0300

    update

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+/*
+const bodyParser = require('body-parser');
+const bdConnection = require('./config/bd')*/
+
+const app = express();
+
+app.use(express.json())
+
+
+
+
+
+
+/*
+route.get('/', (req, res) => {
+    // res.send(`gay kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk`);
+    res.json({
+        message: "mal vindo"
+    });
+})*/
+
+app.listen(3000, () => {
+    console.log("API rodando na porta 3000 e banco na porta 5432")
+})
