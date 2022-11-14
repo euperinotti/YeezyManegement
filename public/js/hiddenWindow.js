1e722c0253e0a6cadd5cfe48a1b3336dc9dbf524
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-11-14 16:18:52 -0300

    update

--- public/js/hiddenWindow.js
+++ public/js/hiddenWindow.js
@@ -1,8 +1,14 @@
 let hiddenWindow = document.querySelector('.hidden-window');
 let addEquipament = document.querySelector('.add-equipament');
+let closeWindow = document.querySelector('.close-window');
 
 console.log('anexado');
 
 addEquipament.addEventListener('click', () => {
     hiddenWindow.style.display = 'flex';
 })
+
+closeWindow.addEventListener('click', () => {
+    hiddenWindow.style.display = 'none';
+})
+
