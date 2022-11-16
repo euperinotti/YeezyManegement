b82e5b80fa1fe5a8683120fdaabef91faadca8c1
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-11-16 13:24:23 -0300

    correcoes nos models

--- public/js/hiddenWindow.js
+++ public/js/hiddenWindow.js
@@ -2,8 +2,6 @@ let hiddenWindow = document.querySelector('.hidden-window');
 let addEquipament = document.querySelector('.add-equipament');
 let closeWindow = document.querySelector('.close-window');
 
-console.log('anexado');
-
 addEquipament.addEventListener('click', () => {
     hiddenWindow.style.display = 'flex';
 })
--- src/controllers/equipamentsController.ts
+++ src/controllers/equipamentsController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
 import path from 'path';
 import { selectedMenu } from '../helpers/menuHelper';
+import { Equipament } from '../models/Equipament';
 
-export const equipaments = (req: Request, res: Response) => {
+export const equipaments = async (req: Request, res: Response) => {
+
+    let teste = await Equipament.create({
+        serialNumber: "123",
+        partNumber: "12312",
+        description: "Está sendo usado segundo",
+        name: "Roteador cisco 2"
+     })
+
+     console.log(JSON.stringify(teste));
+
+
     res.render(path.join(__dirname, '../views/pages/equipaments.ejs'), {
         pageName: 'Equipamentos',
-        menu: selectedMenu('equipaments')
+        menu: selectedMenu('equipaments'),
     });
 }
