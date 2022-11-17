cc5dd6cc28fb7c21968b863a22c4933aabaaf273
Allysom Renczenczen <allysom.renczenczen18@gmail.com>
2022-11-17 16:19:17 -0300

    asdasd

--- src/controllers/clientsController.ts
+++ src/controllers/clientsController.ts
@@ -2,18 +2,24 @@ import { Request, Response } from 'express';
 import path from 'path';
 import { selectedMenu } from '../helpers/menuHelper';
 import { Cliente } from '../models/Client'
+import { Equipament } from '../models/Equipament';
 export const clients = async (req: Request, res: Response) => {
     
-    let teste = await Cliente.create({
-        serialNumber: "123",
-        partNumber: "12312",
-        description: "Está sendo usado segundo",
-        name: "Roteador cisco 2"
+    let mock = await Cliente.create({
+        cnpj: "12345678910111",
+        equipament_idequipamento: "123",
+        name: "Cisco"
      })
+
+     let fk = await Equipament.create({
+        id_idequipamento: "123"
+     })
+     console.log(JSON.stringify(mock));
 
     res.render(path.join(__dirname, '../views/pages/clients.ejs'), {
         pageName: 'Clientes',
         menu: selectedMenu('clients')
     });
    
+    
 }
