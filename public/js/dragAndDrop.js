26496ec60a2b92f44c62278f758eea1b04937386
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-11-28 11:41:44 -0300

    comecando drag n drop

---
+++ public/js/dragAndDrop.js
@@ -0,0 +1,59 @@
+document.querySelector('.side-items')?.addEventListener('click', (e) => {
+    
+    // .target é usado para descobrir qual elemento (e) foi clicado
+    // pelo usuário
+    console.log(e.target);
+
+    // enquanto isso, o currentTarget tem basicamente a mesma finalidade porém ele exibe
+    // o elemento que possui o evento de click
+});
+
+document.querySelectorAll('.occupied-side')?.forEach(area => {
+
+    // Sempre que for necessário criar um evento de drag and drop, é necessário
+    // ter essas 3 funções, drag over, drag leave e drop
+
+    area.addEventListener('dragover', dragOver); // Arrastar um elemento para dentro
+    area.addEventListener('dragleave', dragLeave); // Arrastar um elemento para fora
+    area.addEventListener('drop', drop); // Soltar um elemento
+});
+
+document.querySelectorAll('.item-box')?.forEach(item => {
+
+    // comeco do evento de arrastar
+    item.addEventListener('dragstart', dragStart);
+
+    // final do evento de arrastar
+    item.addEventListener('dragend', dragEnd);
+});
+
+function dragStart(e){
+    e.currentTarget.classList.add('dragging');
+}
+
+function dragEnd(e){
+    e.currentTarget.classList.remove('dragging');
+}
+
+ // Funcções relacionadas a area do elemento
+ 
+
+function dragOver(e){
+
+    // Permite que o elemento seja solto em outro lugar do html
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function dragLeave(e){
+    e.currentTarget.classList.remove('hover');
+}
+
+function drop(e){
+    e.currentTarget.classList.remove('hover');
+
+    if(e.currentTarget.querySelector('.item') == null){
+        let dragItem = document.querySelector('.item.dragging');
+        e.currentTarget.appendChild(dragItem);
+    }
+}
